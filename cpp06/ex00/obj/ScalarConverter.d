4aa06431357dbc6f7fbe19a8ce8755f3713d2496
Am0rA <i.topcu1234567890@gmail.com>
2023-10-23 04:14:21 +0200

    it is not done yet. 4 errors are remaining for compiling

---
+++ cpp06/ex00/obj/ScalarConverter.d
@@ -0,0 +1,5 @@
+obj/ScalarConverter.o: src/ScalarConverter.cpp inc/ScalarConverter.hpp \
+ inc/main.hpp inc/colors.hpp
+inc/ScalarConverter.hpp:
+inc/main.hpp:
+inc/colors.hpp:
---
+++ cpp06/ex00/obj/main.d
@@ -0,0 +1,5 @@
+obj/main.o: src/main.cpp inc/ScalarConverter.hpp inc/main.hpp \
+ inc/colors.hpp
+inc/ScalarConverter.hpp:
+inc/main.hpp:
+inc/colors.hpp:
